61d5cec7c702ef89694f18d5ba0db8ecd7cf0018
suph4rd <suph4rd575@gmail.com>
2021-01-05 00:54:44 +0300

    refactor code according to the pep8

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from app.models import PositionRelations, SalaryPaid, Employee
+from app.models import PositionLevel, SalaryPaid, Employee
 from app.tasks import delete_user
 
 
@@ -10,6 +10,8 @@ def delete_records(modeladmin, request, queryset):
         delete_user.delay(list_user_id)
     else:
         SalaryPaid.objects.filter(employee__in=queryset).delete()
+
+
 delete_records.short_description = "Удалить информацию о заработной плате выбраных сотрудников"
 
 
@@ -28,7 +30,7 @@ class SalaryPaidAdmin(admin.ModelAdmin):
     list_display = ('employee', 'date_paid', 'sum_paid')
 
 
-@admin.register(PositionRelations)
+@admin.register(PositionLevel)
 class PositionRelationsAdmin(admin.ModelAdmin):
     list_select_related = True
     list_display = ('position', 'level')
